70c4639b16df6fc5e12cd2d9db3f0673ebc8f312
artyomalvarez <alvarezmanjarrezjuanjose@gmail.com>
2026-08-27 20:18:53 -0500

    feat: estrutura de carpetas

---
+++ Gestor-de-Tareas.test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Gestor_de_Tareas.test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
